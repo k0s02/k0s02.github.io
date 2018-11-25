ec80e4d8e61a25a3248733413f5b3675eb0e2254
k0s02 <7127573+k0s02@users.noreply.github.com>
2018-11-25 02:25:05 +0000

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,111 @@
+function copyText(target) {
+    var text = document.getElementById(target);
+    text.select();
+    document.execCommand("copy");
+}
+
+function sendData(object){
+    // DO NOT DELETE!!!!!!!!!
+    var textarea = document.getElementById('textarea');
+    textarea.hidden = false;
+    switch(object)
+        {
+            case 'creature_generator':
+                var entry = document.getElementById('c_entry').value;
+                var name = document.getElementById('c_name').value;
+                var subName = document.getElementById('c_subname').value;
+                var gossipid = document.getElementById('c_gossipmenuid').value;
+                var modelid = document.getElementById('c_modelid').value;
+                var factionid = document.getElementById('c_factionid').value;
+                var smartai = document.getElementById('c_smartai');
+                var smartai_option = smartai.options[smartai.selectedIndex].value;
+                var npcflags = document.getElementsByName('npcFlags');
+                var npcflags_length = npcflags.length;
+                var npcflags_value = 0;
+                var flagsextra = document.getElementsByName('flagsExtra');
+                var flagsextra_length = flagsextra.length;
+                var flagsextra_value = 0;
+                var smart = "'" + "" + "'"  ;
+
+                name = "'" + name + "'";
+                subName = "'" + subName + "'";
+                if (smartai_option == 1) {
+                  smart = "'" + "SmartAI" + "'";
+                }
+                for (var i=0; i<npcflags_length; i++) {
+                  if (npcflags[i].checked) {
+                    npcflags_value += Number(npcflags[i].value);
+                  }
+                }
+                for (var j=0; j<flagsextra_length; j++) {
+                  if (flagsextra[j].checked) {
+                    flagsextra_value += Number(flagsextra[j].value);
+                  }
+                }
+
+                var SQL = "INSERT INTO creature_template (entry, name, subname, gossip_menu_id, modelid1, faction, npcflag, flags_extra, AIName) VALUES (" + entry + ", " + name + ", " + subName + ", " + gossipid + ", " + modelid + ", " + factionid + ", " + npcflags_value + ", " + flagsextra_value + ", " + smart + ");\n";
+                textarea.value = SQL;
+                copyText('textarea');
+              break;
+        }
+		if(document.getElementById("c_entry").value == "")
+  {
+		alert("Warning! ENTRY field is be empty!");
+		
+  }	
+}
+
+
+function fillTextTrigger()
+{
+	var triggerButton = document.getElementById("triggerButton");
+	triggerButton.onclick = function()
+	{
+		if(this.onclick)
+		{
+			c_modelid.value = "28998";
+			c_smartai.selectedIndex = true;
+			c_name.value = "Trigger ";
+			c_factionid.value = "35";
+			flagsExtra.checked = true;
+			
+		}
+		
+	}
+	
+}
+	
+window.onload = fillTextTrigger;
+	
+function showData(object)
+{
+      var target = document.getElementById(object);
+      var button;
+  switch (object)
+  {
+      case 'npc_flag':
+        target = document.getElementById('npcFlag_tab');
+        button = document.getElementsByName('npcFlag_button')[0];
+      break;
+      case 'flags_extra':
+        target = document.getElementById('flagsExtra_tab');
+        button = document.getElementsByName('flagsExtra_button')[0];
+      break;
+ 
+    default:
+      alert("This object does not exist in js script");
+    break;
+  }
+  
+  if(target.hidden == true)
+  {
+    target.hidden = false;
+    button.style.color = "lightgreen";
+  }
+  else
+  {
+    target.hidden = true;
+    button.style.color = "inherit";
+  }
+  
+}
