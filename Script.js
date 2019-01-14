8367edf2f1f8d3e28efcca27e29c32c8feaa1239
k0s02 <7127573+k0s02@users.noreply.github.com>
2019-01-14 11:46:07 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -25,6 +25,9 @@ function sendData(object){
                 var flagsextra = document.getElementsByName('flagsExtra');
                 var flagsextra_length = flagsextra.length;
                 var flagsextra_value = 0;
+		var flagsextra2 = document.getElementsByName('flagsExtra2');
+                var flagsextra2_length = flagsextra2.length;
+                var flagsextra2_value = 0;	
                 var smart = "'" + "" + "'"  ;
 
                 name = "'" + name + "'";
@@ -42,8 +45,13 @@ function sendData(object){
                     flagsextra_value += Number(flagsextra[j].value);
                   }
                 }
+		for (var j=0; j<flagsextra2_length; j++) {
+                  if (flagsextra2[j].checked) {
+                    flagsextra2_value += Number(flagsextra2[j].value);
+                  }
+                }	
 
-                var SQL = "INSERT INTO creature_template (entry, name, subname, gossip_menu_id, modelid1, faction, npcflag, flags_extra, AIName) VALUES (" + entry + ", " + name + ", " + subName + ", " + gossipid + ", " + modelid + ", " + factionid + ", " + npcflags_value + ", " + flagsextra_value + ", " + smart + ");\n";
+                var SQL = "INSERT INTO creature_template (entry, name, subname, gossip_menu_id, modelid1, faction, npcflag, flags_extra, flags_extra2 AIName) VALUES (" + entry + ", " + name + ", " + subName + ", " + gossipid + ", " + modelid + ", " + factionid + ", " + npcflags_value + ", " + flagsextra_value + ", " + flagsextra2_value + ", " + smart + ");\n";
                 textarea.value = SQL;
                 copyText('textarea');
               break;
